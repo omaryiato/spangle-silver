0079d43c0011b6c198d131b5285b9f4478c42e64
omar alkhateeb <oalkhateeb1710@gmail.com>
2026-07-08 15:11:45 +0300

    handle client apis

--- app/Http/Controllers/Client/MainOrderController.php
+++ app/Http/Controllers/Client/MainOrderController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers\Client;
 
 use App\Helpers\ResponseHelper;
 use App\Http\Controllers\Controller;
-use App\Http\Resources\OrderDetailResource;
+use App\Http\Resources\OrderResource;
 use App\Services\Client\MainOrderService;
 use Exception;
 use Illuminate\Http\Request;
@@ -22,7 +22,7 @@ class MainOrderController extends Controller
             $order_details = $this->mainOrderService->addNewOrder($request->all());
 
             return ResponseHelper::success(
-                new OrderDetailResource($order_details),
+                new OrderResource($order_details),
                 [
                     'en' => trans('validation.home_page'),
                     'ar' => trans('validation.home_page'),
